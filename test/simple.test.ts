7a9a1b232a1d576a32d3b39003f4de987c030ad4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-11-05 21:27:44 -0500

    trying to write unit tests using orb validate

---
+++ test/simple.test.ts
@@ -0,0 +1,87 @@
+import test from 'ava'
+import { stripIndent } from 'common-tags'
+import * as execa from 'execa'
+import * as tempWrite from 'temp-write'
+
+const validate = (filename: string): Promise<any> => {
+  const cmd = `circleci orb validate ${filename}`
+  return execa.shell(cmd, { stdio: 'inherit' })
+}
+
+const validateConfig = (config: string): Promise<any> => {
+  const filename = tempWrite.sync(config + '\n\n', 'config.yml')
+  return validate(filename)
+}
+
+test('simple', t => {
+  t.plan(0)
+  const config = stripIndent`
+    # simple example workflow
+    version: 2.1
+    orbs:
+      cypress: cypress-io/cypress@0.5.0
+    workflows:
+      build:
+        jobs:
+          - cypress/run
+  `
+  return validateConfig(config)
+})
+
+test('chrome browser', t => {
+  t.plan(0)
+  const config = stripIndent`
+    # simple example workflow
+    version: 2.1
+    orbs:
+      cypress: cypress-io/cypress@0.5.0
+    workflows:
+      build:
+        jobs:
+          - cypress/run:
+              executor: cypress/browsers-chrome69
+              browser: chrome
+  `
+  return validateConfig(config)
+})
+
+test('build and start commands', t => {
+  t.plan(0)
+  const config = stripIndent`
+    # simple example workflow
+    version: 2.1
+    orbs:
+      cypress: cypress-io/cypress@0.5.0
+    workflows:
+      build:
+        jobs:
+          - cypress/run:
+              builds: "npm run build"
+              start: "npm run server"
+  `
+  return validateConfig(config)
+})
+
+test('two jobs with parallel tests', t => {
+  t.plan(0)
+  const config = stripIndent`
+    # simple example workflow
+    version: 2.1
+    orbs:
+      cypress: cypress-io/cypress@0.5.0
+    workflows:
+      build:
+        jobs:
+          - cypress/install:
+              build: 'npm run build'
+          - cypress/run:
+              requires:
+                - cypress/install
+              record: true
+              parallel: true
+              parallelism: 3
+              group: '3x'
+              start: 'npm run server'
+  `
+  return validateConfig(config)
+})
